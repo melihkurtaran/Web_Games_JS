94a2728342d0690c193e3cd1f4aa89f44f86079c
Melih Kurtaran <melihkurtaran@sabanciuniv.edu>
2020-12-26 23:21:28 +0300

    walking and attack sprites added

--- PixiJS/app.js
+++ PixiJS/app.js
@@ -15,25 +15,26 @@ var stage = new PIXI.Container();
 PIXI.loader.add("skull","images/sprite_skull.png").load(setup);
 
 var sprite, sprite2;
+var rect, rect2;
+var idle
 
 function setup(){
     stage.interactive = true;
     
-    var rect = new PIXI.Rectangle(0,0,64,64);
+    rect = new PIXI.Rectangle(0,0,64,64);
+    rect2 = new PIXI.Rectangle(0,0,64,64);
+    
     var texture = PIXI.loader.resources["skull"].texture;
     texture.frame = rect;
-
     sprite = new PIXI.Sprite(texture);
     sprite2 = new PIXI.Sprite(texture);
 
-    var idle = setInterval(function(){
-
-        if(rect.x >= 64 * 4) rect.x = 0;
-        sprite.texture.frame = rect;
-        sprite2.texture.frame = rect;
-        rect.x += 64;
+    idle = setInterval(function(){      
+        if(rect.x >= 64 * 4) rect.x = 0;       
+        sprite.texture.frame = rect;       
+        rect.x += 64;       
     }, 350);
-
+    
     sprite.scale.set(2,2);
     sprite.vx = 10;
     sprite2.scale.set(2,2);
@@ -44,11 +45,10 @@ function setup(){
 
     stage.addChild(sprite);
 
-    sprite2.x += 650;
+    sprite2.x += 650; //arranging position of second player
 
     stage.addChild(sprite2);
 
-    createPlayerSheet();
     animationLoop();
 
 }
@@ -61,40 +61,49 @@ function animationLoop(){
 
 }
 
-function doneLoading(e){
-    createPlayerSheet();
-    createPlayerSheet();
+/*t o change intervals according to players */
+function interval(r){
+    clearInterval(idle);
+    idle = setInterval(function(){      
+    if(r.x >= 64 * 4) r.x = 0;       
+        sprite.texture.frame = r;       
+        r.x += 64;       
+    }, 350);
 }
 
-function createPlayerSheet(){
-    let sshet = new PIXI.BaseTexture.from(PIXI.loader.resources["skull"].url);
-
-    playerSheet["walk"] = [
-        new PIXI.Texture(sshet, new PIXI.Rectangle(0,64,64,128)),
-        new PIXI.Texture(sshet, new PIXI.Rectangle(64,64,132,128)),
-        new PIXI.Texture(sshet, new PIXI.Rectangle(128,64,192,128)),
-        new PIXI.Texture(sshet, new PIXI.Rectangle(192,64,256,128))
-    ];
-    playerSheet["stand"] = [
-        new PIXI.Texture(sshet, new PIXI.Rectangle(0,0,64,64)),
-        new PIXI.Texture(sshet, new PIXI.Rectangle(64,0,132,64)),
-        new PIXI.Texture(sshet, new PIXI.Rectangle(128,0,192,64)),
-        new PIXI.Texture(sshet, new PIXI.Rectangle(192,0,256,64))
-    ];
+/* walking figure */
+function walk(i){
+    if(i==1)
+    {
+        rect = new PIXI.Rectangle(64,64,64,64); 
+        interval(rect);
+        
+    }
+    else if(i==2) 
+    {
+        rect2 = new PIXI.Rectangle(64,64,64,64);  
+        interval(rect2);
+    }
 }
 
-function createPlayer(){
-    player = new PIXI.AnimatedSprite(playerSheet.stand);
-    player.anchor.set(0.5);
-    player.animationSpeed = .5;
-    player.loop = true;
-    stage.addChild(player);
-    player.play();
+/* standing figure */
+function stand(){
+    setTimeout(() => {  rect = new PIXI.Rectangle(0,0,64,64); //wait 1s before standing
+                        rect2 = new PIXI.Rectangle(0,0,64,64);}, 1000);  
 }
 
-function walk(){
-    sprite = new PIXI.AnimatedSprite(playerSheet.walk);
-
+/* attacking figure */
+function attack(i){
+    if(i==1)
+    {
+        rect = new PIXI.Rectangle(128,128,64,64);
+        interval(rect);
+    }
+    else if(i==2) 
+    {
+        rect2 = new PIXI.Rectangle(128,128,64,64);   
+        interval(rect2);
+    }
 }
 
 //keyboard event handlers
@@ -104,16 +113,18 @@ window.addEventListener("keyup",keysUp);
 function keysDown(e){
     keys[e.code] = true;
     console.log(keys);
-    if (keys["KeyD"]) { sprite.x += sprite.vx; }
-    if (keys["KeyA"]) sprite.x -= sprite.vx;
-    if (keys["KeyW"]) sprite.y -= sprite.vx;
-    if (keys["KeyS"]) sprite.y += sprite.vx;
+    if (keys["KeyD"]) { walk(1); sprite.x += sprite.vx;}
+    if (keys["KeyA"]) { walk(1); sprite.x -= sprite.vx; }
+    if (keys["KeyW"]) { walk(1); sprite.y -= sprite.vx; }
+    if (keys["KeyS"]) { walk(1); sprite.y += sprite.vx; }
+    if (keys["KeyE"]) { attack(1); }
 
-    if (keys["ArrowRight"]) sprite2.x += sprite2.vx;
-    if (keys["ArrowLeft"]) sprite2.x -= sprite2.vx;
-    if (keys["ArrowUp"]) sprite2.y -= sprite2.vx;
-    if (keys["ArrowDown"]) sprite2.y += sprite2.vx;
-
+    if (keys["ArrowRight"]) { walk(2); sprite2.x += sprite2.vx;}
+    if (keys["ArrowLeft"]) { walk(2); sprite2.x -= sprite2.vx;}
+    if (keys["ArrowUp"]) { walk(2); sprite2.y -= sprite2.vx;}
+    if (keys["ArrowDown"]) { walk(2); sprite2.y += sprite2.vx;}
+    if (keys["KeyC"]) { attack(2); }
+    stand();
 }
 
 function keysUp(e){
